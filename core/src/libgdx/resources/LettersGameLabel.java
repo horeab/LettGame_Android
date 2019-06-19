34bec1d99aa27021c93c6aef3272627ac7c9fc28
Macbook <macbook@MacBook-Pro-Macbook.local>
2019-06-19 19:15:00 +0300

    Initial commit

---
+++ core/src/libgdx/resources/LettersGameLabel.java
@@ -0,0 +1,25 @@
+package libgdx.resources;
+
+import libgdx.game.Game;
+import libgdx.resources.gamelabel.GameLabelUtils;
+import libgdx.resources.gamelabel.SpecificPropertiesUtils;
+
+public enum LettersGameLabel implements libgdx.resources.gamelabel.GameLabel {
+
+    START_GAME,
+    LEVEL_FINISHED,
+    ALREADY_ADDED,
+    INCORRECT_WORD
+    ;
+
+    @Override
+    public String getText(Object... params) {
+        String language = Game.getInstance().getAppInfoService().getLanguage();
+        return GameLabelUtils.getText(getKey(), language, SpecificPropertiesUtils.getLabelFilePath(), params);
+    }
+
+    @Override
+    public String getKey() {
+        return name().toLowerCase();
+    }
+}
